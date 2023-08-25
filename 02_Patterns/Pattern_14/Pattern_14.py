6626bbd72d611b23d726aced313e29e4bb444963
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2023-08-25 21:35:53 +0530

    Added Solution

---
+++ 02_Patterns/Pattern_14/Pattern_14.py
@@ -0,0 +1,21 @@
+class Solution:
+    def printTriangle(self, N):
+        for i in range(1, N+1):
+            # inner loop for columns
+            # ord() to get ASCII code of char
+            for j in range(ord('A'), ord('A')+i):
+                # chr() used to get a string representing of a character which points to a Unicode code integer eg: ch(65)=A
+                print(chr(j), end="")
+            print()
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
