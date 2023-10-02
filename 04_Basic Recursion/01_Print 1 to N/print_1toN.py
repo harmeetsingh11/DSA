9c6e1f4e1f5252e4ec839ed9ef97ae4365582a12
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2023-10-02 20:00:22 +0530

    Added Solution

--- 04_Basic Recursion/01_Print 1 to N/print_1toN.py
+++ 04_Basic Recursion/01_Print 1 to N/print_1toN.py
@@ -1,18 +1,27 @@
 from typing import List
 
 
-def printNos(x: int, ans: List[int]):
+def recursiveFunction(x: int, ans: List[int]) -> None:
     if x == 0:
         return
     else:
         # TRUST the function that it will print 1 to n-1
-        printNos(x-1, ans)
+        recursiveFunction(x-1, ans)
         # Then, it will print last element (nth)
         ans.append(x)
-        return ans
+
+
+def printNos(x: int) -> List[int]:
+    # declaring an empty list
+    ans = []
+
+    # calling recursive function
+    recursiveFunction(x, ans)
+
+    # returning the result
+    return ans
 
 
 if __name__ == "__main__":
     n = int(input())
-    ans = []
-    print(printNos(n, ans))
+    print(printNos(n))
