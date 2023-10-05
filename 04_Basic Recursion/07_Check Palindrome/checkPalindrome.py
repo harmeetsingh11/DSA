65f139c5cc4e03126410cd3e3814a1afb3868ad7
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2023-10-05 22:04:57 +0530

    Added Solution

---
+++ 04_Basic Recursion/07_Check Palindrome/checkPalindrome.py
@@ -0,0 +1,17 @@
+def recursiveFunction(s: str, start: int, end: int) -> str:
+    if start >= end:
+        return "true"
+    if s[start] != s[end]:
+        return "false"
+    else:
+        return recursiveFunction(s, start+1, end-1)
+
+
+def isPalindrome(string: str) -> bool:
+    return recursiveFunction(string, 0, len(string)-1)
+
+
+# Driver Code
+if __name__ == "__main__":
+    s = "madam"
+    print(isPalindrome(s).lower())
