b4e0d2e0eca678fbab831d43ffe9f1b611e260ea
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2023-12-21 21:10:42 +0530

    Added recursive solution

--- 04_Basic Recursion/07_Check Palindrome/checkPalindrome.py
+++ 04_Basic Recursion/07_Check Palindrome/checkPalindrome.py
@@ -1,9 +1,16 @@
 def recursiveFunction(s: str, start: int, end: int) -> str:
+
+    # Base Condition
+    # If start exceeds end or half of the string, means all the elements 
+    # are compared, we return true.
     if start >= end:
         return "true"
-    # if first and last character is not same, then not palindrome
+    
+    # If first and last character is not same, then not palindrome
     if s[start] != s[end]:
         return "false"
+    
+    # If both characters are same, increment i and check start+1 and end-1.
     else:
         return recursiveFunction(s, start+1, end-1)
 
@@ -14,5 +21,7 @@ def isPalindrome(string: str) -> bool:
 
 # Driver Code
 if __name__ == "__main__":
+    # Example String
     s = "madam"
     print(isPalindrome(s).lower())
+
