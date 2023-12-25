00e918c8a42a71c898724e5263cabc5eccce3ac6
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2023-12-25 21:37:42 +0530

    Added solution

--- 04_Basic Recursion/08_Print Fibonacci Series/fibonacci.py
+++ 04_Basic Recursion/08_Print Fibonacci Series/fibonacci.py
@@ -1,23 +1,19 @@
+# Instead of printing the Fibonacci series till N, we’re going to print the Nth Fibonacci number using functional recursion with multiple function calls.
 
+# fib(n) = fib(n-1) + fib(n-2) 
 
-# fib(n) = fib(n-1)+fib(n-2) 
-
-from typing import List
 def generateFibonacciSeries(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
-    else:
-        fib_series = generateFibonacciSeries(n - 1)
-        fib_series.append(fib_series[-1] + fib_series[-2])
-        return fib_series
+    # base case
+    # series starting from 0 
+    if n <=1:
+        return n
+    
+    last = generateFibonacciSeries(n-1)
+    secondLast = generateFibonacciSeries(n-2)
+
+    return last + secondLast
 
 if __name__ == "__main__":
-    n = int(input("Enter the value of n: "))
-    fibonacci_series = generateFibonacciSeries(n)
-    print(f"Fibonacci Series up to {n}: {fibonacci_series}")
-
+    n = int(input())
+    print(generateFibonacciSeries(n))
     
