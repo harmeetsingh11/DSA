77c7970c9f2e5636b32a2c8d65e9372f001f2686
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-03-18 22:34:41 +0530

    chore: add alternate solution (using Map)

--- 05_Basic Hashing/02_Count freq of characters in string/count-freq-of-characters-in-string.js
+++ 05_Basic Hashing/02_Count freq of characters in string/count-freq-of-characters-in-string.js
@@ -1,3 +1,4 @@
+// METHOD 1: USING OBJECT
 function countCharacterFrequency(str) {
   // Initialize an empty object to store the frequency of each character
   const frequency = {};
@@ -17,7 +18,26 @@ function countCharacterFrequency(str) {
   return frequency;
 }
 
-// Example usage:
+// METHOD 2: USING MAP()
+function countCharacterFrequency(str) {
+  // Initialize a new Map object to store the frequency of each character
+  const frequency = new Map();
+
+  // Iterate over each character of the input string
+  for (const char of str) {
+    // Skip spaces
+    if (char !== ' ') {
+      // Check if the current character exists as a key in the frequency map
+      // If it does not exist, initialize its value to 0 and increment it by 1
+      // If it already exists, retrieve its current frequency and increment it by 1
+      frequency.set(char, (frequency.get(char) || 0) + 1);
+    }
+  }
+
+  // Return the frequency map containing the frequency of each character
+  return frequency;
+}
+
 const str = 'hello world';
 const result = countCharacterFrequency(str);
 console.log(result);
