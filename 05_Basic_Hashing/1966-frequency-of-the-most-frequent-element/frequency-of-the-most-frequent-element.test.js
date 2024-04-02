91f357efc82b8145204cf14ba92b389ea8ba424f
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-04-02 21:55:40 +0530

    test: update test description

--- 05_Basic_Hashing/1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.test.js
+++ 05_Basic_Hashing/1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.test.js
@@ -1,15 +1,15 @@
 import maxFrequency from './frequency-of-the-most-frequent-element';
 
 describe('maxFrequency function', () => {
-  it('Example 1: [1, 2, 4], k = 5', () => {
+  it('Should return 3 for nums = [1, 2, 4], k = 5', () => {
     expect(maxFrequency([1, 2, 4], 5)).toBe(3);
   });
 
-  it('Example 2: [1, 4, 8, 13], k = 5', () => {
+  it('Should return 2 for nums = [1, 4, 8, 13], k = 5', () => {
     expect(maxFrequency([1, 4, 8, 13], 5)).toBe(2);
   });
 
-  it('Example 3: [3, 9, 6], k = 2', () => {
+  it('Should return 1 for nums = [3, 9, 6], k = 2', () => {
     expect(maxFrequency([3, 9, 6], 2)).toBe(1);
   });
 });
