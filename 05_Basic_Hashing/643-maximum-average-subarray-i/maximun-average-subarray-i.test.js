c6dd82909a7e885c180a92a6c60117d6c799f415
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-04-06 21:07:26 +0530

    test: add unit test for findMaxAverage function using jest

---
+++ 05_Basic_Hashing/643-maximum-average-subarray-i/maximun-average-subarray-i.test.js
@@ -0,0 +1,19 @@
+import findMaxAverage from './maximum-average-subarray-i';
+
+describe('findMaxAverage', () => {
+  it('should return the correct maximum average for the given input', () => {
+    expect(findMaxAverage([1, 12, -5, -6, 50, 3], 4)).toBeCloseTo(12.75);
+  });
+
+  it('should return the correct value when array has only one element', () => {
+    expect(findMaxAverage([5], 1)).toBeCloseTo(5.0);
+  });
+
+  it('should handle arrays with negative numbers', () => {
+    expect(findMaxAverage([-1, -2, -3, -4, -5], 3)).toBeCloseTo(-2.0);
+  });
+
+  it('should handle arrays with all positive numbers', () => {
+    expect(findMaxAverage([10, 20, 30, 40, 50], 2)).toBeCloseTo(45.0);
+  });
+});
