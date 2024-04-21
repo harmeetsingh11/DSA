562fd616de118b00017fc5870c25404f038a08d3
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-04-21 21:49:07 +0530

    feat: implement invertedPyramid function in JavaScript

---
+++ 02_Patterns/Pattern_08/Pattern_08.js
@@ -0,0 +1,16 @@
+function invertedPyramid(n) {
+  // outer loop fo number of lines
+  for (let i = 1; i <= n; i++) {
+    // for spaces
+    let spaces = ' '.repeat(i);
+
+    // for stars
+    let stars = '*'.repeat(2 * n - (2 * i - 1));
+
+    // Each row consists of spaces, stars, and then spaces, printing as per the pattern.
+    console.log(spaces + stars + spaces);
+  }
+}
+
+// Call the function
+// invertedPyramid(10);
