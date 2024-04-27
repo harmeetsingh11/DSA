d4e42ac6205fed83a89bffded0d91eaf55ad4a21
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-04-27 23:55:57 +0530

    feat: implement zeroOnesTrianglePattern function in JavaScript

--- 02_Patterns/Pattern_11/Pattern_11.js
+++ 02_Patterns/Pattern_11/Pattern_11.js
@@ -1,15 +1,15 @@
-function zeroOnesTrianglePattern(N) {
+function zeroOnesTrianglePattern(n) {
   // Outer loop for number of rows/lines
-  for (let i = 1; i <= lines; i++) {
-    let line = '';
+  for (let i = 1; i <= n; i++) {
+    let row = '';
 
     // Inner loop for columns to print the values in the row
     for (let j = 1; j <= i; j++) {
       // If the sum of line index and column index is even, print 1, else print 0
-      line += (i + j) % 2 === 0 ? '1 ' : '0 ';
+      row += (i + j) % 2 === 0 ? '1 ' : '0 ';
     }
 
-    console.log(line);
+    console.log(row);
   }
 }
 
