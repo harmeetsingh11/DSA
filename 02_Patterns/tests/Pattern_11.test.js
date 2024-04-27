72e066036a9faa788e32c6ea42c845f79f44459e
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-04-28 00:06:52 +0530

    feat: update Jest test cases for zeroOnesTrianglePattern function

--- 02_Patterns/tests/Pattern_11.test.js
+++ 02_Patterns/tests/Pattern_11.test.js
@@ -6,11 +6,11 @@ describe('zeroOnesTrianglePattern', () => {
     zeroOnesTrianglePattern(5);
     expect(consoleSpy).toHaveBeenCalledTimes(5);
     expect(consoleSpy.mock.calls).toEqual([
-      ['0 '],
-      ['10 '],
-      ['010 '],
-      ['1010 '],
-      ['01010 '],
+      ['1 '],
+      ['0 1 '],
+      ['1 0 1 '],
+      ['0 1 0 1 '],
+      ['1 0 1 0 1 '],
     ]);
     consoleSpy.mockRestore();
   });
@@ -20,13 +20,13 @@ describe('zeroOnesTrianglePattern', () => {
     zeroOnesTrianglePattern(7);
     expect(consoleSpy).toHaveBeenCalledTimes(7);
     expect(consoleSpy.mock.calls).toEqual([
-      ['0 '],
-      ['10 '],
-      ['010 '],
-      ['1010 '],
-      ['01010 '],
-      ['101010 '],
-      ['0101010 '],
+      ['1 '],
+      ['0 1 '],
+      ['1 0 1 '],
+      ['0 1 0 1 '],
+      ['1 0 1 0 1 '],
+      ['0 1 0 1 0 1 '],
+      ['1 0 1 0 1 0 1 '],
     ]);
     consoleSpy.mockRestore();
   });
@@ -36,15 +36,15 @@ describe('zeroOnesTrianglePattern', () => {
     zeroOnesTrianglePattern(9);
     expect(consoleSpy).toHaveBeenCalledTimes(9);
     expect(consoleSpy.mock.calls).toEqual([
-      ['0 '],
-      ['10 '],
-      ['010 '],
-      ['1010 '],
-      ['01010 '],
-      ['101010 '],
-      ['0101010 '],
-      ['10101010 '],
-      ['010101010 '],
+      ['1 '],
+      ['0 1 '],
+      ['1 0 1 '],
+      ['0 1 0 1 '],
+      ['1 0 1 0 1 '],
+      ['0 1 0 1 0 1 '],
+      ['1 0 1 0 1 0 1 '],
+      ['0 1 0 1 0 1 0 1 '],
+      ['1 0 1 0 1 0 1 0 1 '],
     ]);
     consoleSpy.mockRestore();
   });
