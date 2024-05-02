43eb4c64006d2c45074b6cfabab5a74b8de82edd
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-05-02 21:15:47 +0530

    feat: implement hollowDiamondStarPattern function in JavaScript

---
+++ 02_Patterns/Pattern_19/Pattern_19.js
@@ -0,0 +1,53 @@
+function hollowDiamondStarPattern(n) {
+  // UPPER TRIANGLE
+  // Outer loop for number of lines
+  for (let i = 1; i <= n; i++) {
+    let row = '';
+
+    // Stars
+    for (let j = 1; j <= n + 1 - i; j++) {
+      row += '*';
+    }
+
+    // Spaces
+    for (let j = 1; j <= 2 * i - 2; j++) {
+      row += ' ';
+    }
+
+    // Stars
+    for (let j = 1; j <= n + 1 - i; j++) {
+      row += '*';
+    }
+
+    console.log(row);
+  }
+
+  // LOWER TRIANGLE
+  // Outer loop for number of lines
+  for (let i = 1; i <= n; i++) {
+    let row = '';
+
+    // Stars
+    for (let j = 1; j <= i; j++) {
+      row += '*';
+    }
+
+    // Spaces
+    for (let j = 1; j <= 2 * n - 2 * i; j++) {
+      row += ' ';
+    }
+
+    // Stars
+    for (let j = 1; j <= i; j++) {
+      row += '*';
+    }
+
+    console.log(row);
+  }
+}
+
+// Call the function
+hollowDiamondStarPattern(15);
+
+// Export the function for testing
+module.exports = hollowDiamondStarPattern;
