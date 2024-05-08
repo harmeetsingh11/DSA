025edac4bee93785615e4965b74f99b2b3f9663c
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-05-08 17:19:00 +0530

    feat: implement optimal approach for checkPrime function in JavaScript

---
+++ 03_Basic_Maths/tests/07_optimal_checkPrime.test.js
@@ -0,0 +1,23 @@
+function checkPrimeOptimal(n) {
+  let count = 0;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      count++;
+      if (i !== n / i) {
+        count++;
+      }
+    }
+  }
+
+  if (count === 2) {
+    return 'YES';
+  } else {
+    return 'NO';
+  }
+}
+
+// call the function using sample input
+console.log(checkPrimeOptimal(6));
+
+// Export the function for testing
+module.exports = checkPrimeOptimal;
