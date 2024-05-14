f99c8035b109051a2e0294e8b4a40f72088787a4
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-05-14 20:58:35 +0530

    feat: implement optimal(iterative) approach for sumFirstNumbers function in JavaScript

---
+++ 04_Basic_Recursion/04_Sum Of First N Numbers/04_optimalSumFirstN.js
@@ -0,0 +1,10 @@
+function sumFirstNoptimal(n) {
+  // Iterative approach using mathematical formula
+  return (n * (n + 1)) / 2;
+}
+
+//   Call the function using sample inputs
+// console.log(sumFirstNoptimal(5));
+
+// Export the function for testing
+module.exports = sumFirstNoptimal;
