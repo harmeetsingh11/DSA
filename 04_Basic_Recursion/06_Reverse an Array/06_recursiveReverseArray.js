440f3995a182b2f5df4c426367e6fa1125b524aa
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-05-15 22:43:55 +0530

    feat: implement recursive approach for reverseArray function in JavaScript

---
+++ 04_Basic_Recursion/06_Reverse an Array/06_recursiveReverseArray.js
@@ -0,0 +1,19 @@
+function reverseArray(nums, start = 0, end = nums.length - 1) {
+  // Base case: If start is greater than or equal to end, return the array as it is reversed
+  if (start >= end) {
+    return nums;
+  } else {
+    // Swap the elements at the start and end indices using destructuring assignment
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+
+    // TRUST the function: reverseArray function will reverse next pair of elements
+    // Move the start pointer towards the right and end pointer towards the left
+    reverseArray(nums, start + 1, end - 1);
+  }
+
+  // Return the reversed array
+  return nums.join(' ');
+}
+
+// call the function using sample inputs
+// console.log(reverseArray([1, 2, 3, 4, 5]));
